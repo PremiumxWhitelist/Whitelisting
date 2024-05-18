bff26a303b81c1208a9b576f3fe0c921714ff221
PremiumxWhitelist <iceyworkshoponline@gmail.com>
2024-05-18 22:05:00 +0200

    Update Whitelist-Premium-Gui.lua

--- Whitelist/Whitelist-Premium-Gui.lua
+++ Whitelist/Whitelist-Premium-Gui.lua
@@ -1,18 +1,18 @@
 --TEMPLATE: [000000000] = true,   -- ROBLOX_ID_HERE | DISCOR_ID_HERE
 --EXAMPLE: [2520431055] = true,  -- xF4ir | 534409864181448707
 local scriptWhitelist = {
-    [2520431055] = true,  -- xF4ir
-    [871092912] = true,   -- Mrpupbro
-    [1491503204] = true,   -- robinbackup5
-    [1357681509] = true,   -- robinbackup1
+    [2520431055] = true,  -- xF4ir | 534409864181448707
+    [871092912] = true,   -- Mrpupbro | 534409864181448707
+    [1491503204] = true,   -- robinbackup5 | 534409864181448707
+    [1357763058] = true,   -- robinbackup3 | 534409864181448707
+    [1357681509] = true,   -- robinbackup1 | 534409864181448707
     [3352870732] = true,   -- 2ill_M | 1188996017656446986
-    [3647872448] = true,   -- pendalove3
-    [3710265398] = true,   -- snorlax01047
-    [5421324166] = true,   -- chiefkeefdedicated
-    [5525658953] = true,   -- Hate_candy112
-    [2514442488] = true,   -- Effyf3
-    [2514442488] = true,   -- ROBLOX_ID_HERE | DISCOR_ID_HERE
-    [1357763058] = true,   -- robinbackup3
+    [3647872448] = true,   -- pendalove3 | 1227026443175727224
+    [3710265398] = true,   -- snorlax01047 | 1157663141090558023
+    [5421324166] = true,   -- chiefkeefdedicated | 1157663141090558023
+    [5525658953] = true,   -- Hate_candy112 | 1213602112295010456
+    [2514442488] = true,   -- Effyf3 | 1213602112295010456
+    
 }
 
 local function executeScript()
